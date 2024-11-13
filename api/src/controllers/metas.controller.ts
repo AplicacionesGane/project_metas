b53f480cb0062a4a0e5e575144d6ae2f9f8bc529
llOrtegall <IvanOrtega_97@hotmail.com>
2024-11-13 10:29:01 -0500

    fix: update metasDelDia to include PROMO1 in product sales calculations

--- api/src/controllers/metas.controller.ts
+++ api/src/controllers/metas.controller.ts
@@ -21,36 +21,37 @@ export const metasDelDia = async (req: Request, res: Response) => {
 
   try {
     await MetasProducts.sync()
-    const metaDiaProdc = await MetasProducts.findOne({
-      attributes: ['CHANCE', 'PAGAMAS', 'PAGATODO', 'GANE5', 'PATA_MILLONARIA', 'DOBLECHANCE', 'CHANCE_MILLONARIO'],
+    const ventaProductos = await MetasProducts.findOne({
+      attributes: ['CHANCE', 'PAGAMAS', 'PAGATODO', 'GANE5', 'PATA_MILLONARIA', 'DOBLECHANCE', 'CHANCE_MILLONARIO', 'PROMO1'],
       where: { SUCURSAL: escape(codigo), FECHA: fn('CURDATE') }
     })
 
-    if (!metaDiaProdc) return res.status(404).json({ message: 'No se encontraron metas para el codigo y fecha proporcionados' })
+    if (!ventaProductos) return res.status(404).json({ message: 'No se encontraron metas para el codigo y fecha proporcionados' })
 
     // TODO: mejorar esta logica se podria crear una funcion reutilizable para sumar los productos
-    const sumaProducts = (
-      metaDiaProdc.dataValues.CHANCE + metaDiaProdc.dataValues.PAGAMAS +
-      metaDiaProdc.dataValues.PAGATODO + metaDiaProdc.dataValues.GANE5 +
-      metaDiaProdc.dataValues.PATA_MILLONARIA + metaDiaProdc.dataValues.DOBLECHANCE +
-      metaDiaProdc.dataValues.CHANCE_MILLONARIO
+    // ?? Suman productos hace referencia a la venta actual del dia
+    const ventaActualSumProductos = (
+      ventaProductos.dataValues.CHANCE + ventaProductos.dataValues.PAGAMAS +
+      ventaProductos.dataValues.PAGATODO + ventaProductos.dataValues.GANE5 +
+      ventaProductos.dataValues.PATA_MILLONARIA + ventaProductos.dataValues.DOBLECHANCE +
+      ventaProductos.dataValues.CHANCE_MILLONARIO + ventaProductos.dataValues.PROMO1
     )
 
-    const ventaActual = await MetasProducts.findOne({
-      attributes: ['PROMEDIO_DIARIO_CHANCE', 'PROMEDIO_DIARIO_PAGAMAS', 'PROMEDIO_DIARIO_PAGATODO', 'PROMEDIO_DIARIO_GANE5', 'PROMEDIO_DIARIO_PATAMI', 'PROMEDIO_DIARIO_DOBLECHANCE', 'PROMEDIO_DIARIO_CHMILL'],
+    const promediosDiarios = await MetasProducts.findOne({
+      attributes: ['PROMEDIO_DIARIO_CHANCE', 'PROMEDIO_DIARIO_PAGAMAS', 'PROMEDIO_DIARIO_PAGATODO', 'PROMEDIO_DIARIO_GANE5', 'PROMEDIO_DIARIO_PATAMI', 'PROMEDIO_DIARIO_DOBLECHANCE', 'PROMEDIO_DIARIO_CHMILL', 'META_PROMO1'],
       where: { SUCURSAL: escape(codigo), FECHA: fn('CURDATE') }
     })
 
     const aspiracionDia = (
-      ventaActual?.dataValues.PROMEDIO_DIARIO_CHANCE + ventaActual?.dataValues.PROMEDIO_DIARIO_PAGAMAS +
-      ventaActual?.dataValues.PROMEDIO_DIARIO_PAGATODO + ventaActual?.dataValues.PROMEDIO_DIARIO_GANE5 +
-      ventaActual?.dataValues.PROMEDIO_DIARIO_PATAMI + ventaActual?.dataValues.PROMEDIO_DIARIO_DOBLECHANCE +
-      ventaActual?.dataValues.PROMEDIO_DIARIO_CHMILL
+      promediosDiarios?.dataValues.PROMEDIO_DIARIO_CHANCE + promediosDiarios?.dataValues.PROMEDIO_DIARIO_PAGAMAS +
+      promediosDiarios?.dataValues.PROMEDIO_DIARIO_PAGATODO + promediosDiarios?.dataValues.PROMEDIO_DIARIO_GANE5 +
+      promediosDiarios?.dataValues.PROMEDIO_DIARIO_PATAMI + promediosDiarios?.dataValues.PROMEDIO_DIARIO_DOBLECHANCE +
+      promediosDiarios?.dataValues.PROMEDIO_DIARIO_CHMILL + promediosDiarios?.dataValues.META_PROMO1
     )
 
-    const porcentajeCumplimiento = calcularPorcentaje(sumaProducts, aspiracionDia)
+    const porcentajeCumplimiento = calcularPorcentaje(ventaActualSumProductos, aspiracionDia)
 
-    return res.status(200).json({ venta_actual: sumaProducts, aspiracion: aspiracionDia, cumplimiento: porcentajeCumplimiento })
+    return res.status(200).json({ venta_actual: ventaActualSumProductos, aspiracion: aspiracionDia, cumplimiento: porcentajeCumplimiento })
   } catch (error) {
     return res.status(500).json({ message: 'Error al obtener las metas', error })
   }
