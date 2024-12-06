4ebde4ec48557e092d4b6af11e0ff685b616bdb7
llOrtegall <IvanOrtega_97@hotmail.com>
2024-12-06 10:14:37 -0500

    updated caterorias works ok

--- client/src/components/ui/RenderCategoria.tsx
+++ client/src/components/ui/RenderCategoria.tsx
@@ -1,15 +1,20 @@
 import { memo, useMemo } from 'react'
 
-type Categoria = 'diamante' | 'zafiro' | 'oro' | 'plata' | 'bronce'
+type Categoria = 'diamante' | 'diamante1a' | 'diamante2a'| 'diamante3a'| 'diamante4a'| 'diamante5a' | 'zafiro' | 'oro' | 'plata' | 'bronce'
 
-function RenderCategoria ({ cat: categoria, ver: version }: { cat: string, ver: string }) {
+function RenderCategoria ({ cat: categoria }: { cat: string }) {
   const imageMap = useMemo(() => ({
-    diamante: `/diamante${version}.webp`,
+    diamante: '/diamante.webp',
+    diamante1a: '/diamante1a.webp',
+    diamante2a: '/diamante2a.webp',
+    diamante3a: '/diamante3a.webp',
+    diamante4a: '/diamante4a.webp',
+    diamante5a: '/diamante5a.webp',
     zafiro: '/zafiro.webp',
     oro: '/oro.webp',
     plata: '/plata.webp',
     bronce: '/bronce.webp'
-  }), [version])
+  }), [])
 
   const RenderImagen = ({ cat } : {cat: Categoria}) => {
     const src = imageMap[cat]
