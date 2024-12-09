3ce362b193c5873ce8eb999bab98ddb77ea9a28b
llOrtegall <IvanOrtega_97@hotmail.com>
2024-12-09 16:29:21 -0500

    Refactor NavLinkItem component: update props and improve icon rendering

--- client/src/components/ui/NavLinkItem.tsx
+++ client/src/components/ui/NavLinkItem.tsx
@@ -1,21 +1,21 @@
+import { RemixiconComponentType } from '@remixicon/react';
 import { NavLink } from 'react-router-dom'
-import React from 'react'
 
 interface NavLinkItemProps {
-  path: string
-  icon: React.ElementType
-  label: string
+  IconRender: RemixiconComponentType;
+  titleName: string;
+  routeTo: string;
 }
 
-export const NavLinkItem = ({ icon, label, path }: NavLinkItemProps) => {
-  const IconComponent = icon
-  const styles = 'flex items-center gap-3 3xl:gap-4 text-xs lg:text-sm lg:text-xl 2xl:text-2xl '
-  const stylesSelected = 'flex items-center gap-3 3xl:gap-4 text-blue-700 dark:text-yellow-500 text-xs lg:text-sm lg:text-xl 2xl:text-2xl '
-
+export const NavLinkItem = ({ IconRender, routeTo, titleName }: NavLinkItemProps) => {
   return (
-    <NavLink className={({ isActive }) => isActive ? `${stylesSelected}` : `${styles}`} to={path}>
-      <IconComponent />
-      <span className=''>{label}</span>
+    <NavLink className={({ isActive }) => isActive
+      ? 'flex items-center gap-3 3xl:gap-4 text-blue-700 dark:text-yellow-500 text-xs lg:text-xl 2xl:text-2xl'
+      : 'flex items-center gap-3 3xl:gap-4 text-xs lg:text-xl 2xl:text-2xl'}
+      to={routeTo}
+    >
+      <IconRender />
+      <span className=''>{titleName}</span>
     </NavLink>
   )
 }
