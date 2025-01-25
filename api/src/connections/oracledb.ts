7cf077dccbbb9cb6bee2b15e09b0c3256b1372c7
llOrtegall <IvanOrtega_97@hotmail.com>
2025-01-25 09:30:50 -0500

    refactor: simplify OracleDB connection configuration by using environment variables directly

--- api/src/connections/oracledb.ts
+++ api/src/connections/oracledb.ts
@@ -1,21 +1,15 @@
+import { DB_ORACLE_DIR, DB_ORACLE_DIR_TNS, DB_ORACLE_NAME, DB_ORACLE_PASS, DB_ORACLE_USER } from '../config/oracleEnv';
 import oracledb from 'oracledb';
 
-const USER_DB = process.env.DB_ORACLE_USER as string;
-const PASSWORD_DB = process.env.DB_ORACLE_PASS as string;
-const NAME_DB = process.env.DB_ORACLE_NAME as string;
-const DIR_DB = process.env.DB_ORACLE_DIR as string;
-
-const DIR_TNS = process.env.DB_ORACLE_DIR_TNS as string;
-
-oracledb.initOracleClient({ libDir: DIR_DB });
+oracledb.initOracleClient({ libDir: DB_ORACLE_DIR });
 
 export async function connectionOracle() {
   try {
     const pool = await oracledb.createPool({
-      user: USER_DB,
-      password: PASSWORD_DB,
-      configDir: DIR_TNS,
-      connectString: NAME_DB
+      user: DB_ORACLE_USER,
+      password: DB_ORACLE_PASS,
+      configDir: DB_ORACLE_DIR_TNS,
+      connectString: DB_ORACLE_NAME
     });
 
     return pool.getConnection();
