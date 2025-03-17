44679e8a2f76a55e42937362acc9b5fc2d0cbf6b
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-17 09:51:49 -0500

    se quita asyn validate funtions

--- api/src/schemas/validate.ts
+++ api/src/schemas/validate.ts
@@ -1,18 +1,21 @@
+import { BaseError } from '../utils/baseError';
 import { z } from 'zod';
 
 const loginSchema = z.object({
   username: z.string()
-    .min(5, { message: "Usuario debe contener 5 caracteres como mínimo" })
-    .max(16, { message: "Usuario debe contener 16 caracteres como máximo" })
+    .min(8, { message: "Usuario debe contener al menos 8 caracteres" })
+    .max(16, { message: "Usuario debe contener como máximo 16 caracteres" })
     .refine((val) => val.startsWith('CV'), { message: "El usuario debe iniciar con 'CV' en mayúscula" }),
   password: z.string()
-  .min(3, { message: "Contraseña debe contener más de 3 caracteres" })
-})
+    .min(3, { message: "Contraseña debe contener más de 3 caracteres" })
+});
 
-export const validateCredentials = async (credentials: unknown) => {
-  const { success, data, error } = await loginSchema.safeParseAsync(credentials);
+export const validateCredentials = (credentials: unknown) => {
+  const { success, data, error } = loginSchema.safeParse(credentials);
 
-  if(!success) throw new Error(error.errors[0].message);
+  if (!success) {
+    throw new BaseError(error.errors[0].message, 400, true)
+  }
 
   return data;
-}
+};
