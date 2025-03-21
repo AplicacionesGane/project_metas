677d7bff1fece8f98c65a4c8aa40fc76078b0c69
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-21 12:09:38 -0500

    Update HistLoginRegister to insert or update login records with version control

--- api/src/services/historialLogin.ts
+++ api/src/services/historialLogin.ts
@@ -1,5 +1,5 @@
 import { HistLogin } from '../models/histLoginPowerbi';
-import { fn } from 'sequelize';
+import { fn, literal } from 'sequelize';
 
 export const HistLoginRegister = async (username: string, sucursal: number) => {
   //TODO: Insertar un dato de login si no existe en la tabla de historialLogin del día actual con la función CURDATE
@@ -13,9 +13,13 @@ export const HistLoginRegister = async (username: string, sucursal: number) => {
         USERNAME: username,
         SUCURSAL: sucursal
       });
+      console.log('Registro de login insertado');
     } else {
-      console.log(histLogin);
-      console.log('Ya existe un registro de login para el usuario para el día de hoy');
+      const updateNumber = histLogin.dataValues.VERSION === 0 ? 1 : 0;
+      await HistLogin.update({ VERSION: updateNumber }, {
+        where: { USERNAME: username, SUCURSAL: sucursal, FECHA_LOGIN: literal('CURDATE()') }
+      })
+      console.log('Registro de login actualizado');
     }
   } catch (error) {
     console.error('Error al insertar el registro de login:', error);
