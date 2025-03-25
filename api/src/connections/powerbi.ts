b04be987069fed5ae25ecf0acc9d49975de9d997
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-25 15:11:55 -0500

    Refactor Power BI database connection to use environment variables from config

--- api/src/connections/powerbi.ts
+++ api/src/connections/powerbi.ts
@@ -1,14 +1,10 @@
-import { Sequelize } from 'sequelize'
+import { DB_POWERBI_DATABASE, DB_POWERBI_HOST, DB_POWERBI_PASSWORD, DB_POWERBI_PORT, DB_POWERBI_USER } from '../config/envPowerBi';
+import { Sequelize } from 'sequelize';
 
-const POWERBI_DB_USER = process.env.DB_POWERBI_USER!
-const POWERBI_DB_PASSWORD = process.env.DB_POWERBI_PASSWORD!
-const POWERBI_DB_HOST = process.env.DB_POWERBI_HOST!
-const POWERBI_DB_PORT = process.env.DB_POWERBI_PORT!
-const POWERBI_DB_DATABASE = process.env.DB_POWERBI_DATABASE!
 
-const powerBi = new Sequelize(POWERBI_DB_DATABASE, POWERBI_DB_USER, POWERBI_DB_PASSWORD, {
-  host: POWERBI_DB_HOST,
-  port: parseInt(POWERBI_DB_PORT),
+const powerBi = new Sequelize(DB_POWERBI_DATABASE, DB_POWERBI_USER, DB_POWERBI_PASSWORD, {
+  host: DB_POWERBI_HOST,
+  port: DB_POWERBI_PORT,
   dialect: 'mysql',
   timezone: '-05:00',
   logging: false
