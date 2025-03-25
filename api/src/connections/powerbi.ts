cb6c2d91652db47c6f172e3fd3a9311bdfa4b32f
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-25 15:13:25 -0500

    refactor spaces on files

--- api/src/connections/powerbi.ts
+++ api/src/connections/powerbi.ts
@@ -1,7 +1,6 @@
 import { DB_POWERBI_DATABASE, DB_POWERBI_HOST, DB_POWERBI_PASSWORD, DB_POWERBI_PORT, DB_POWERBI_USER } from '../config/envPowerBi';
 import { Sequelize } from 'sequelize';
 
-
 const powerBi = new Sequelize(DB_POWERBI_DATABASE, DB_POWERBI_USER, DB_POWERBI_PASSWORD, {
   host: DB_POWERBI_HOST,
   port: DB_POWERBI_PORT,
--- api/src/connections/test_92.ts
+++ api/src/connections/test_92.ts
@@ -6,7 +6,6 @@ const TEST_70_DB_HOST = process.env.DB_TEST_70_HOST!
 const TEST_70_DB_PORT = process.env.DB_TEST_70_PORT!
 const TEST_70_DB_DATABASE = process.env.DB_TEST_70_DATABASE!
 
-
 const Test70Conn = new Sequelize(TEST_70_DB_DATABASE, TEST_70_DB_USER, TEST_70_DB_PASSWORD, {
   host: TEST_70_DB_HOST,
   port: parseInt(TEST_70_DB_PORT),
