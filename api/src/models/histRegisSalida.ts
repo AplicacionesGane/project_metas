c3776dc81b39f65db11e59db1a124626273f51cd
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-27 08:12:09 -0500

    Add HistRegisSalida model for tracking user logout records

---
+++ api/src/models/histRegisSalida.ts
@@ -0,0 +1,28 @@
+import { DataTypes, Model, InferAttributes, InferCreationAttributes } from 'sequelize';
+import { powerBi } from '../connections/powerbi';
+
+export class HistRegisSalida extends Model<InferAttributes<HistRegisSalida>, InferCreationAttributes<HistRegisSalida>> {
+  declare TIPO: string;
+  declare USERNAME: string;
+  declare SUCURSAL: number;
+  declare FECHA_LOGOUT: Date;
+  declare BLOQREG: number;
+  declare FECHACREATE?: Date;
+  declare FECHAUPDATE?: Date;
+  declare VERSION: number;
+}
+
+HistRegisSalida.init({
+  TIPO: { type: DataTypes.ENUM, values:['SALIDA', 'SALIDAINT', 'SALIDATEMP'], allowNull: false, primaryKey: true },
+  USERNAME: { type: DataTypes.STRING(15), allowNull: false, primaryKey: true },
+  SUCURSAL: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true },
+  FECHA_LOGOUT: { type: DataTypes.DATEONLY, allowNull: false, defaultValue: DataTypes.NOW },
+  BLOQREG: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+  FECHACREATE: { type: DataTypes.DATE, allowNull: true, defaultValue: DataTypes.NOW },
+  FECHAUPDATE: { type: DataTypes.DATE, allowNull: true, defaultValue: DataTypes.NOW },
+  VERSION: { type: DataTypes.INTEGER, allowNull: true, defaultValue: 0 }
+}, {
+  sequelize: powerBi,
+  tableName: 'HIST_REGISTROSALIDAS',
+  timestamps: false
+});
