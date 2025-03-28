345014a1d827317b4f976a8b795b79f5995ddbe7
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-28 15:48:42 -0500

    Refactor SugeridosPage component to simplify data handling and remove unused code

--- client/src/pages/Sugeridos.tsx
+++ client/src/pages/Sugeridos.tsx
@@ -1,17 +1,8 @@
-import { useSugeridos } from '../hooks/useSugeridos'
-import { useSugeridos2 } from '../hooks/useSugeridos2'
-
 import { ProgressSugerido } from '../components/ProgressSugerido'
-import { ConsultarBoletasGanadas } from '../components/BoletaGanada'
-import { useAuth } from '../auth/AuthContext'
+import { useSugeridos } from '../hooks/useSugeridos'
 
 function SugeridosPage () {
-  const { profileData } = useAuth()
-
-  const username = `CV${profileData?.user.DOCUMENTO}`
-
-  const { data } = useSugeridos(profileData?.sucursal.CODIGO!, username, profileData?.sucursal.ZONA!)
-  const { data2 } = useSugeridos2(profileData?.sucursal.CODIGO!, username, profileData?.sucursal.ZONA!)
+  const { data } = useSugeridos()
 
   return (
     <section className='flex flex-col mx-2'>
@@ -21,22 +12,7 @@ function SugeridosPage () {
         <article className='2xl:grid-cols-2 gap-2 2xl:gap-6'>
           {data && <ProgressSugerido data={data} />}
         </article>
-
-        {
-          data && data2?.VTA_SUGERIDO && data.VTA_SUGERIDO > data.META_SUG1
-            ? (
-              <article className='2xl:grid-cols-2 gap-2 2xl:gap-6'>
-                <ProgressSugerido data={data2} />
-              </article>
-              )
-            : null
-        }
       </main>
-
-      <footer className='py-2'>
-        <ConsultarBoletasGanadas codigo={profileData?.sucursal.CODIGO!} user={username} names={profileData?.user.NOMBRES || 'ninguno'} />
-      </footer>
-
     </section>
   )
 }
