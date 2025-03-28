1ddf633df9fc979d1a0ec8a6a2743a64c381bc70
llOrtegall <IvanOrtega_97@hotmail.com>
2025-03-28 16:19:57 -0500

    Enhance CardSugerido component with improved styling and layout for better readability

--- client/src/components/CardSugerido.tsx
+++ client/src/components/CardSugerido.tsx
@@ -11,7 +11,7 @@ export function CardSugerido({ sugerido }: { sugerido: Sugeridos }) {
   const variantColor = getColorVariant(parseFloat(percentage));
 
   return (
-    <Card className={`w-1/2 ${bgColor}`} key={sugerido.ID}>
+    <Card className={`w-1/2 ${bgColor} shadow-md`} key={sugerido.ID}>
       <article className='flex justify-between py-2'>
         <h2 className='font-semibold text-lg'>Producto Sugerido:</h2>
         <span>-</span>
@@ -19,19 +19,22 @@ export function CardSugerido({ sugerido }: { sugerido: Sugeridos }) {
       </article>
 
       <section className='flex justify-between py-2'>
-        <article className='flex gap-2 items-center text-gray-600'>
+        <article className='flex gap-2 items-center text-gray-700'>
           <p> Venta Actual:</p>
-          <Badge variant='success'>{formatPrice(sugerido.VTA_SUGERIDO)}</Badge>
+          <Badge variant='success' className='shadow-md'>{formatPrice(sugerido.VTA_SUGERIDO)}</Badge>
         </article>
 
-        <article className='flex gap-2 items-center text-gray-600'>
+        <article className='flex gap-2 items-center text-gray-700'>
           <p> Valor Sugerido:</p>
-          <Badge variant='warning'>{formatPrice(sugerido.META_VALOR)}</Badge>
+          <Badge variant='warning' className='shadow-md'>{formatPrice(sugerido.META_VALOR)}</Badge>
         </article>
       </section>
 
-      <article className='flex flex-col items-center'>
-        <p> <span>Progeso </span> &bull; {percentage} %</p>
+      <article className=''>
+        <div className='flex gap-2 items-center justify-center text-gray-700'>
+          <p>Progreso Actual: </p>
+          <Badge variant='default' className='shadow'>{percentage} %</Badge>
+        </div>
         <ProgressBar value={parseInt(percentage)} variant={variantColor} className='mt-3' />
       </article>
 
