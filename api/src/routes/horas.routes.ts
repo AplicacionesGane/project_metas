594846a0c68d9e9f26959789dafcdd1b2c7a981d
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-04 08:39:13 -0500

    Remove horas.routes.ts file to clean up unused code

--- api/src/routes/horas.routes.ts
+++
@@ -1,6 +0,0 @@
-import { MetaHoraSucursal } from '../controllers/horas.controller';
-import { Router } from 'express';
-
-export const RouteHoras = Router()
-
-RouteHoras.get('/ventaxhora', MetaHoraSucursal)
