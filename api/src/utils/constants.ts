59823d4895b66ec52477926b96655805310474e1
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-08 09:25:21 -0500

    Fix product names in PRODUCTOS_DATA for consistency by removing underscores

--- api/src/utils/constants.ts
+++ api/src/utils/constants.ts
@@ -9,7 +9,7 @@ export const PRODUCTOS_DATA = [
   },
   {
     codigos: ['136', '300', '137', '301', '303', '139', '304', '140', '305', '306', '307'],
-    nombre: 'PAGA_TODO'
+    nombre: 'PAGATODOY'
   },
   {
     codigos: ['183', '184', '36', '37', '38', '39', '40', '41', '53', '54', '55', '56', '57', '58'],
@@ -17,15 +17,15 @@ export const PRODUCTOS_DATA = [
   },
   {
     codigos: ['10', '11', '113', '12', '13', '14', '15', '16', '17', '18', '19', '59', '60', '61', '62', '63', '114'],
-    nombre: 'DOBLE_CHANCE'
+    nombre: 'DOBLECHANCE'
   },
   {
     codigos: ['133', '134', '153', '20', '21', '22', '23', '3450', '50', '51', '52'],
-    nombre: 'PAGA_MAS'
+    nombre: 'PAGAMAS'
   },
   {
     codigos: ['227', '228', '284', '285'],
-    nombre: 'PATA_MILLONARIA'
+    nombre: 'PATAMILLONARIA'
   },
   {
     codigos: ['561'],
@@ -33,7 +33,7 @@ export const PRODUCTOS_DATA = [
   },
   {
     codigos: ['122'],
-    nombre: 'CHANCE_MILLONARIO'
+    nombre: 'CHANCEMILLONARIO'
   },
   {
     codigos: ['2180'],
