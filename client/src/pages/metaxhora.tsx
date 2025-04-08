6eca58f21783be2a68508c16c1abf54008057030
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-08 09:20:22 -0500

    Add "Venta Esta Hora" display to MetaXhora component and update layout

--- client/src/pages/metaxhora.tsx
+++ client/src/pages/metaxhora.tsx
@@ -7,6 +7,7 @@ interface ObjectEsperado {
   venta: number;
   hora: string;
   estimado: number;
+  ventaEstaHora: number;
 }
 
 export default function MetaXhora() {
@@ -37,7 +38,36 @@ export default function MetaXhora() {
           `$${Intl.NumberFormat("us").format(number).toString()}`
         }
         onValueChange={(v) => console.log(v)}
-        />
+      />
+
+      <div className="bg-gray-50 p-6 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-extrabold text-gray-800 mb-6 text-center">Meta por Hora</h1>
+
+        {
+          data.map((item, index) => (
+            item.venta !== 0 && item.ventaEstaHora !== 0 ? (
+              <div
+                key={index}
+                className="grid grid-cols-4 gap-6 p-6 border border-gray-200 rounded-lg shadow-md bg-white hover:shadow-lg transition-shadow duration-300 m-4"
+              >
+                <h2 className="text-xl font-bold text-blue-600 col-span-1">{item.hora}</h2>
+                <p className="text-base text-gray-700 col-span-1">
+                  <span className="font-semibold text-gray-900">Venta Acumulada:</span>
+                  {` $${Intl.NumberFormat("CO").format(item.venta).toString()}`}
+                </p>
+                <p className="text-base text-gray-700 col-span-1">
+                  <span className="font-semibold text-gray-900">Estimado:</span>
+                  {` $${Intl.NumberFormat("CO").format(item.estimado).toString()}`}
+                </p>
+                <p className="text-base text-gray-700 col-span-1">
+                  <span className="font-semibold text-gray-900">Venta Esta Hora:</span>
+                  {` $${Intl.NumberFormat("CO").format(item.ventaEstaHora).toString()}`}
+                </p>
+              </div>
+            ) : null
+          ))
+        }
+      </div>
     </div>
   );
 }
