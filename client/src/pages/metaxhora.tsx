bf596fd06e1057b0b75d2abea30e5a38bfbe35b5
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-09 13:49:47 -0500

    Remove unnecessary sucursal parameter from /metaxhoras API request

--- client/src/pages/metaxhora.tsx
+++ client/src/pages/metaxhora.tsx
@@ -16,7 +16,7 @@ export default function MetaXhora() {
   const [data, setData] = useState<ObjectEsperado[]>([]);
 
   useEffect(() => {
-    axios.get<ObjectEsperado[]>(`/metaxhoras`, { params: { producto, sucursal } })
+    axios.get<ObjectEsperado[]>(`/metaxhoras`, { params: { producto } })
       .then((response) => {
         console.log('Response:', response.data);
         setData(response.data);
