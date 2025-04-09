c76b51cbf27fa43aa606ddda4dedbdd156a7e8d3
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-09 13:49:21 -0500

    Refactor ventaxhorasController to validate query parameters and simplify response structure

--- api/src/controllers/ventaxhoras.controller.ts
+++ api/src/controllers/ventaxhoras.controller.ts
@@ -1,28 +1,56 @@
 import { Ventaxhoras } from '../models/ventahoras';
 import { Productos } from '../models/productospwbi';
 import { Request, Response } from 'express';
-import { fn, Op } from 'sequelize';
+import { fn } from 'sequelize';
+import { z } from 'zod';
+
+const paramsSchema = z.object({
+  producto: z.string(),
+});
 
 export const ventaxhorasController = async (req: Request, res: Response) => {
+  const { success, data, error } = paramsSchema.safeParse(req.query);
+
+  if (!success) {
+    res.status(400).json({ message: 'Invalid query parameters', error });
+    return
+  }
+
   try {
     const results = await Ventaxhoras.findAll({
-      attributes: ['HORA', 'LOGIN', 'VTAH', 'VTAPH', 'ASPIRACIONH'],
-      include: [
-        {
-          model: Productos,
-          attributes: ['NOMBRE', 'CATEGORIACOMERCIAL', 'VERSION'], 
-          where: {
-            VERSION: { [Op.ne]: '0' },
-          },
-        },
-      ],
-      where: {
+      attributes: ['HORA', 'VTAH'],
+      include: [{
+        model: Productos,
+        attributes: ['VERSION'],
+        where: { VERSION: 'DOBLECHANCE' }
+      }],
+      where:{
         SUCURSAL: 39825,
         FECHA: fn('CURDATE'),
-      },
-    });
+      }
+    })
 
-    res.status(200).json(results);
+    const mapItems = results.map((item) => {
+      const { HORA, VTAH } = item;
+      return {
+        HORA,
+        VTAH
+      }
+    })
+
+    // group by HORA and sum VTAH
+    const groupedItems = mapItems.reduce((acc, item) => {
+      const { HORA, VTAH } = item;
+      if (!acc[HORA]) {
+        acc[HORA] = { HORA, VTAH: 0 };
+      }
+      acc[HORA].VTAH += VTAH;
+      return acc;
+    }, {} as Record<string, { HORA: number; VTAH: number }>);
+
+    
+    res.status(200).json(groupedItems);
+    return
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ message: 'Internal server error' });
