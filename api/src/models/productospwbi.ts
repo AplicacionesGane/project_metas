980ef7a6e60500032a5cf27a088e808b50437544
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-09 08:53:08 -0500

    Add Productos model to define database structure for PRODUCTOS

---
+++ api/src/models/productospwbi.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Model, InferAttributes, InferCreationAttributes } from 'sequelize';
+import { powerBi } from '../connections/powerbi';
+
+class Productos extends Model<InferAttributes<Productos>, InferCreationAttributes<Productos>> {
+  declare CODIGO: string;
+  declare NOMBRE: string;
+  declare TIPOJUEGO: string;
+  declare VERSION: string;
+  declare CATEGORIACOMERCIAL: string;
+  declare IVA: number;
+  declare EXPLOTACION: number;
+}
+
+Productos.init({
+  CODIGO: { type: DataTypes.STRING(10), allowNull: false, primaryKey: true, },
+  NOMBRE: { type: DataTypes.STRING(60), allowNull: true, },
+  TIPOJUEGO: { type: DataTypes.STRING(60), allowNull: true, },
+  VERSION: { type: DataTypes.STRING(20), allowNull: true, },
+  CATEGORIACOMERCIAL: { type: DataTypes.STRING(40), allowNull: true, },
+  IVA: { type: DataTypes.DECIMAL(5, 2).UNSIGNED, allowNull: true, defaultValue: 0.00, },
+  EXPLOTACION: { type: DataTypes.DECIMAL(5, 2).UNSIGNED, allowNull: true, defaultValue: 0.00, },
+}, {
+  sequelize: powerBi,
+  tableName: 'PRODUCTOS',
+  timestamps: false,
+}
+);
