dd9199f3a0e48f9617ba6426ebc7f9a48c5e621c
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-10 10:25:14 -0500

    Add PRODUCTOS_DATA constant for product information

---
+++ api/src/utils/constants.ts
@@ -0,0 +1,42 @@
+export const PRODUCTOS_DATA = [
+  {
+    codigos: [1, 2, 3],
+    nombre: 'CHANCE'
+  },
+  {
+    codigos: [102, 620, 109, 621],
+    nombre: 'GANE5'
+  },
+  {
+    codigos: [136, 300, 137, 301, 303, 139, 304, 140, 305, 306, 307],
+    nombre: 'PAGA_TODO'
+  },
+  {
+    codigos: [183, 184, 36, 37, 38, 39, 40, 41, 53, 54, 55, 56, 57, 58],
+    nombre: 'ASTRO'
+  },
+  {
+    codigos: [10, 11, 113, 12, 13, 14, 15, 16, 17, 18, 19, 59, 60, 61, 62, 63, 114],
+    nombre: 'DOBLECHANCE'
+  },
+  {
+    codigos: [133, 134, 153, 20, 21, 22, 23, 3450, 50, 51, 52],
+    nombre: 'PAGA_MAS'
+  },
+  {
+    codigos: [227, 228, 284, 285],
+    nombre: 'PATA_MILLONARIA'
+  },
+  {
+    codigos: [561],
+    nombre: 'SUPERWIN'
+  },
+  {
+    codigos: [122],
+    nombre: 'CHANCE_MILLONARIO'
+  },
+  {
+    codigos: [2180],
+    nombre: 'MILOTO'
+  }
+]
