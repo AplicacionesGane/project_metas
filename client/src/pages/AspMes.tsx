b5b9141def808042882a1d49bd52b67a6c451f87
llOrtegall <IvanOrtega_97@hotmail.com>
2025-04-30 11:15:49 -0500

    refactor AspMesPage component to standardize logout function and improve data fetching logic

--- client/src/pages/AspMes.tsx
+++ client/src/pages/AspMes.tsx
@@ -1,28 +1,29 @@
-import { BarraProgressProduct } from '../components/ProgresoProducto'
-import { useFecthMetasData } from '../hooks/useFetchData'
-import { sortData } from '../utils/funtions'
-import { useMemo, useState } from 'react'
-import { HeaderComponent } from '../components/ui/headerComponent'
-import { useAuth } from '../auth/AuthContext'
+import { BarraProgressProduct } from '../components/ProgresoProducto';
+import { HeaderComponent } from '../components/ui/headerComponent';
+import { useFetchData } from '../hooks/useFetch';
+import { useAuth } from '../auth/AuthContext';
+import { sortData } from '../utils/funtions';
+import { MetasProI } from '../types/Metas';
+import { useMemo, useState } from 'react';
 
-function AspMesPage () {
-  const { funLogOut} = useAuth()
+function AspMesPage() {
+  const { logout } = useAuth()
 
-  const { data, isLoading, close } = useFecthMetasData('/cumpMesAct')
+  const { data, loading, closeSesion } = useFetchData<MetasProI>('/cumpMesAct')
   const [isAscending, setIsAscending] = useState(false)
 
   const sortedData = useMemo(() => {
     return Array.isArray(data) ? sortData(data, isAscending) : []
   }, [data, isAscending])
 
-  if(close) {
-    funLogOut()
+  if (closeSesion) {
+    logout()
     return null
   }
 
   return (
     <section className='relative'>
-      <HeaderComponent setIsAscending={setIsAscending} isAscending={isAscending} isLoading={isLoading} text='Mes Actual' />
+      <HeaderComponent setIsAscending={setIsAscending} isAscending={isAscending} isLoading={loading} text='Mes Actual' />
       <article className='grid grid-cols-2 gap-2 px-1 2xl:grid-cols-3 3xl:grid-cols-4'>
         {data && (
           sortedData.map(meta => (
