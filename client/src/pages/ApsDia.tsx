7d1894efd0b759cbbf65d3cb235f1632a925c925
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-04-30 00:43:05 -0500

    Refactor AspDiaPage to use useFetchData hook with improved error handling and session management

--- client/src/pages/ApsDia.tsx
+++ client/src/pages/ApsDia.tsx
@@ -1,28 +1,39 @@
 import { BarraProgressProduct } from '../components/ProgresoProducto'
 import { HeaderComponent } from '../components/ui/headerComponent'
-import { useFecthMetasData } from '../hooks/useFetchData'
-import { sortData } from '../utils/funtions'
+import { useFetchData } from '../hooks/useFetch'
+import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../auth/AuthContext'
+import { sortData } from '../utils/funtions'
+import { MetasProI } from '../types/Metas'
 import { useMemo, useState } from 'react'
 
 function AspDiaPage() {
-  const { funLogOut } = useAuth()
-
-  const { data, isLoading, close } = useFecthMetasData('/cumpDiaProd')
+  const { data, error, loading, closeSesion } = useFetchData<MetasProI>('/cumpDiaProd')
+  const navigate = useNavigate()
   const [isAscending, setIsAscending] = useState(false)
+  const { funLogOut } = useAuth()
 
   const sortedData = useMemo(() => {
     return Array.isArray(data) ? sortData(data, isAscending) : []
   }, [data, isAscending])
 
-  if (close) {
+  if(closeSesion){
     funLogOut()
+    navigate('/')
     return null
+  }
+
+  if(error){
+    return (
+      <section>
+        Se presentó un error al solicitar la Información, intenta cerras sesión y volver a iniciar.
+      </section>
+    )
   }
 
   return (
     <section className='px-1'>
-      <HeaderComponent setIsAscending={setIsAscending} isLoading={isLoading} isAscending={isAscending} text='Día Actual' />
+      <HeaderComponent setIsAscending={setIsAscending} isLoading={loading} isAscending={isAscending} text='Día Actual' />
       <article className='grid grid-cols-2 gap-2 px-1 2xl:grid-cols-3 3xl:grid-cols-4'>
         {
           sortedData.map(meta =>
