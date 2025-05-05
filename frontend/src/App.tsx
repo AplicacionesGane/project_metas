e24e425c1104e2579564e5b4b575b925b43f253f
llOrtegall <IvanOrtega_97@hotmail.com>
2025-05-05 10:35:25 -0500

    vite install shadcn react 19 and new folder to proyect

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <section>
+      <h1>hola</h1>
+      <Button>Enviar</Button>
+    </section>
+  )
+}
+
+export default App
