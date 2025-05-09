55bf49848016a569dec7cd2a3e76425755e0ad67
llOrtegall <IvanOrtega_97@hotmail.com>
2025-05-09 11:07:14 -0500

    url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { RouterMain } from '@/routes';
 import { StrictMode } from 'react';
 import axios from 'axios';
 
-axios.defaults.baseURL = import.meta.env.VITE_URL_API || '/api'
+axios.defaults.baseURL = import.meta.env.VITE_URL_API || 'http://api-metas:80/api/v1'
 axios.defaults.withCredentials = true
 
 createRoot(document.getElementById('root')!).render(
