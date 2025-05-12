aad5fdac3d9b7196e8e55e91648c3d7c4e880c26
llOrtegall <IvanOrtega_97@hotmail.com>
2025-05-12 08:08:09 -0500

    agregado show animation to barra progress

--- frontend/src/components/ui/progress.tsx
+++ frontend/src/components/ui/progress.tsx
@@ -1,12 +1,23 @@
 import { Root, Indicator } from "@radix-ui/react-progress";
 import { colorBackground } from "@/lib/funtions";
+import { useEffect, useState } from "react";
 
 function Progress({ value }: { value: number }) {
+  const [displayValue, setDisplayValue] = useState(0);
+
+  useEffect(() => {
+    setDisplayValue(0);
+    const timeout = setTimeout(() => {
+      setDisplayValue(value);
+    }, 200);
+
+    return () => clearTimeout(timeout);
+  }, [value]);
   return (
-    <Root className={`relative h-2 w-full overflow-hidden rounded-full shadow ${colorBackground(value)}`}>
+    <Root className={`relative h-2 w-full overflow-hidden rounded-full shadow ${colorBackground(displayValue)}`}>
       <Indicator
-        className={`text-black h-full w-full transition-all duration-700 ease-in-out ${colorBackground(value, 'dark')}`}
-        style={{ width: `${value}%` }}
+        className={`text-black h-full w-full transition-all duration-1000 ease-in-out ${colorBackground(displayValue, 'dark')}`}
+        style={{ width: `${displayValue}%` }}
       />
     </Root>
   );
