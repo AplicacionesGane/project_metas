ab1999c1fff7fc443eff3df163883a2c03ac1d10
llOrtegall <IvanOrtega_97@hotmail.com>
2025-05-12 07:47:47 -0500

    modify chart bar, add card components to description

--- frontend/src/components/bar-char.tsx
+++ frontend/src/components/bar-char.tsx
@@ -1,7 +1,23 @@
-import { type ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart"
-import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+import { TrendingUp } from "lucide-react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
 import type { MetaXhoraData } from "@/app/meta-hora";
-import { Card } from "@/components/ui/card";
 
 const chartConfig = {
   desktop: {
@@ -14,48 +30,56 @@ const chartConfig = {
   },
 } satisfies ChartConfig
 
-export default function CharVentaHora2({ data }: { data: MetaXhoraData[] }) {
+export default function BarChar({ data }: { data: MetaXhoraData[] }) {
   return (
     <Card>
-      <ChartContainer config={chartConfig}>
-        <AreaChart
-          accessibilityLayer
-          data={data}
-          margin={{
-            left: 12,
-            right: 12,
-          }}
-        >
-          <CartesianGrid vertical={false} />
-          <XAxis
-            dataKey="HORA"
-            tickLine={false}
-            axisLine={false}
-            tickMargin={8}
-            tickFormatter={(value) => value}
-          />
-          <ChartTooltip
-            cursor={false}
-            content={<ChartTooltipContent indicator="dot" />}
-          />
-          <Area
-            dataKey="VTAH"
-            type="natural"
-            fill="var(--chart-8)"
-            fillOpacity={0.4}
-            stroke="var(--chart-8)"
-            stackId="a"
-          />
-          <Area
-            dataKey="METAH"
-            type="natural"
-            fill="var(--chart-7)"
-            fillOpacity={0.4}
-            stroke="var(--chart-7)"
-            stackId="a"
-          />
-        </AreaChart>
-      </ChartContainer>
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">Gráfico de Lineas <TrendingUp /></CardTitle>
+        <CardDescription>{new Date().toLocaleDateString()}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={data}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="HORA"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+            <Line
+              dataKey="VTA_HORA"
+              type="monotone"
+              stroke="var(--chart-7)"
+              strokeWidth={2}
+              dot={false}
+            />
+            <Line
+              dataKey="META_HORA"
+              type="monotone"
+              stroke="var(--chart-8)"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter>
+        <div className="flex w-full items-start gap-2 text-sm">
+          <div className="flex items-center gap-2 leading-none text-muted-foreground">
+              Gráficos usados desde la biblioteca Shadcn, usando la librería Recharts - licencia MIT
+          </div>
+        </div>
+      </CardFooter>
     </Card>
   )
 }
