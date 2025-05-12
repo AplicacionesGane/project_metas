834df83332eb4442e9c8b832fe59d80a7b38f7db
Servired - Multired <aplicaciones@grupomultired.com.co>
2025-05-12 12:33:35 -0500

    Update bar-char.tsx

--- frontend/src/components/bar-char.tsx
+++ frontend/src/components/bar-char.tsx
@@ -76,7 +76,9 @@ export default function BarChar({ data }: { data: MetaXhoraData[] }) {
       <CardFooter>
         <div className="flex w-full items-start gap-2 text-sm">
           <div className="flex items-center gap-2 leading-none text-muted-foreground">
-            Componentes Gráficos de <span className="font-semibold text-gray-800">Shadcn UI</span>, la cual usá en su core la librería Recharts - Licencia MIT
+            Componentes gráficos de 
+            <span className="font-semibold text-gray-800">Shadcn UI,</span> los cuales utilizan en su núcleo la biblioteca Recharts, licenciada bajo 
+            <span className="font-semibold text-gray-800">MIT.</span>
           </div>
         </div>
       </CardFooter>
