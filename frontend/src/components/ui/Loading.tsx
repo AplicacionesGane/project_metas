b9162b5c564b343cfe50be0f7886457da2f99782
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-17 11:09:50 -0500

    reafactor loading component to fix error on view

--- frontend/src/components/ui/Loading.tsx
+++ frontend/src/components/ui/Loading.tsx
@@ -1,5 +1,9 @@
 export const Loading = () => (
-  <div className="h-screen w-full flex items-center justify-center">
+  <div className="
+  fixed top-[50%] left-[50%] z-40 grid max-w-[calc(100%-2rem)] 
+  translate-x-[-50%] 
+  translate-y-[-50%] 
+  gap-4 rounded-lg border p-6 shadow-lg duration-200">
     <div className="flex flex-col gap-2 items-center">
       <div className="relative w-16 h-16">
         <div className="absolute inset-0 animate-spin rounded-full bg-primary/10 border-4 border-t-transparent"></div>
