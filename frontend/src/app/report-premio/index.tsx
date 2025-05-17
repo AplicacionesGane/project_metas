d247f9eb2b615c51462a70c837ecc2627bbe3ffb
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-05-17 09:48:50 -0500

    work in form to send new report premio

--- frontend/src/app/report-premio/index.tsx
+++ frontend/src/app/report-premio/index.tsx
@@ -1,17 +1,32 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DialogUserCreate } from "@/components/create-new-report";
 import { useAuth } from "@/hooks/useAuth"
+import { useState } from "react";
 
-import { Navigate, redirect } from "react-router";
+import { Navigate } from "react-router";
 
 export default function ReportPremio() {
   const { user } = useAuth();
+  const [reload, setReload] = useState(false);
 
   if(user?.maquinas === false) {
     return <Navigate to="/" replace />
   }
 
+  const handleReload = () => {
+    setReload(!reload)
+  }
+
   return (
-    <div>
-      <h1>Report Premio</h1>
-    </div>
+    <Card className="h-[calc(100vh-7rem)]">
+      <CardHeader className="flex items-center justify-between">
+        <CardTitle>Reporte Premios Máquinas</CardTitle>
+        <DialogUserCreate reload={handleReload} />
+      </CardHeader>
+
+      <CardContent>
+        Lista Reportes generados día actual
+      </CardContent>
+    </Card>
   )
 }
---
+++ frontend/src/components/create-new-report.tsx
@@ -0,0 +1,28 @@
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import { Loading } from '@/components/ui/Loading';
+import { lazy, Suspense, useState } from 'react';
+import { PlusIcon } from 'lucide-react';
+
+const LazyDialogContent = lazy(() => import('./new-report-content'));
+
+export const DialogUserCreate = ({ reload }: { reload: () => void }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenChange = (openDialog: boolean) => {
+    setIsOpen(openDialog)
+  }
+
+  return (
+    <Dialog onOpenChange={v => handleOpenChange(v)} open={isOpen}>
+      <DialogTrigger className='cursor-pointer bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded-md flex items-center gap-2'>
+        <PlusIcon className="size-4" />
+        Enviar Nuevo Reporte
+      </DialogTrigger>
+      {isOpen && (
+        <Suspense fallback={<Loading />}>
+          <LazyDialogContent funClose={handleOpenChange} reload={reload}/>
+        </Suspense>
+      )}
+    </Dialog>
+  );
+};
